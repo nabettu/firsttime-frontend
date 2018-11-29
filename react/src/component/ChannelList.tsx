0f299e0100ef01356c65f900418ba0f46c8e71ba
nabettu <tatsuaki.w@gmail.com>
2018-11-29 23:15:03 +0900

    react router入れるまで

---
+++ react/src/component/ChannelList.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { Menu, Icon } from "semantic-ui-react";
+
+const channels = ["general", "random"];
+
+export const ChannelList = () => {
+  return (
+    <Menu inverted vertical fixed={"left"}>
+      <Menu.Item as={NavLink} to={"/"}>
+        home
+      </Menu.Item>
+      <Menu.Item>
+        Channels
+        <Icon name="list" />
+        <Menu.Menu>
+          {channels.map(channel => (
+            <Menu.Item
+              key={channel}
+              as={NavLink}
+              name={channel}
+              to={{ pathname: `/channels/${channel}` }}
+            >
+              {channel}
+            </Menu.Item>
+          ))}
+        </Menu.Menu>
+      </Menu.Item>
+    </Menu>
+  );
+};
